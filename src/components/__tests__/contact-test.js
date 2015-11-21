933ebc9c8bf65859c7721917b31fb48dff15de90
Jim Wharton <jimwharton@overstock.com>
2015-11-21 11:22:35 -0700

    add global expect

--- src/components/__tests__/contact-test.js
+++ src/components/__tests__/contact-test.js
@@ -1,7 +1,18 @@
+import {createRenderer} from 'react-addons-test-utils';
 import Contact from '../contact';
 
 describe('test that tests work', function() {
     it('should pass', function() {
         expect(true).to.be.true;
+    });
+});
+
+describe('Contact component', () => {
+    it('renders correctly', () => {
+        let renderer = createRenderer();
+        renderer.render(<Contact />);
+        let actualElement = renderer.getRenderOutput();
+        let expectedElement = <div><Contact /></div>;
+        expect(actualElement).to.eql(expectedElement);
     });
 });
