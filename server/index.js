fabd2b01f3c2ea0b67163d6ddb18a52594a6a2e4
Jim Wharton <jimwharton@overstock.com>
2015-11-21 21:59:46 -0700

    update paths to views dir

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
+import path from 'path';
 import express from 'express';
+import handlebars from 'express-handlebars';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Contact from './generated/contact';
 
 const app = express();
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
+// Static assets
+app.use(express.static(path.resolve(__dirname, '../dist')));
 app.get('/contact', (request, response) => {
-    response.send(ReactDOMServer.renderToString(<Contact />));
+    response.render('app', {
+        app: ReactDOMServer.renderToString(<Contact />)
+    });
 });
 
 app.listen(3000, () => console.log('Server running'));
